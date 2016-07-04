5e881ec681eac423768ec6613d70012e4481bde3
yaojiadong <yaojiadong@gmail.com>
2016-07-04 20:26:39 +0200

    Add files via upload

---
+++ task_sheet/2/sheet2_templates/ses_lcd.h
@@ -0,0 +1,54 @@
+#ifndef SES_LCD_H_
+#define SES_LCD_H_
+
+/*INCLUDES-------------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdint.h>
+
+
+/*EXTERNALS------------------------------------------------------------------*/
+
+/**
+ * File descriptor for lcd. You can use this in conjunction with fprintf.
+ * Example fprintf(lcdout, "Hello World %d\n",2012);
+ *
+ */
+extern FILE* lcdout;
+
+
+/*PROTOTYPES-----------------------------------------------------------------*/
+
+/**
+ * Initializes the display print facility.
+ */
+void lcd_init();
+
+/**
+ * Jumps character cursor to specified position. The position
+ * of the cursor determines the position of the next character
+ * which is written.
+ *
+ * @param p		horizontal position of cursor
+ * @param r		row of cursor
+ */
+void lcd_setCursor(uint8_t p, uint8_t r);
+
+
+/**
+ * Writes a character to the position of the cursor. Afterwards
+ * the cursor position is increased by one.
+ *
+ * @param chr 	character to write to display
+ *
+ */
+void lcd_putc(char chr);
+
+
+/**
+ * Clears the whole display.
+ */
+void lcd_clear();
+
+
+
+#endif /* SES_LCD_H_ */
---
+++ task_sheet/2/sheet2_templates/ses_led.c
@@ -0,0 +1,66 @@
+/* INCLUDES ******************************************************************/
+
+#include "ses_common.h"
+#include "ses_led.h"
+
+/* DEFINES & MACROS **********************************************************/
+
+// LED wiring on SES board
+#define LED_RED_PORT       	PORTG
+#define LED_RED_PIN         	1
+
+#define LED_YELLOW_PORT 	PORTF
+#define LED_YELLOW_PIN      	7
+
+#define LED_GREEN_PORT 		PORTF
+#define LED_GREEN_PIN       	6
+
+/* FUNCTION DEFINITION *******************************************************/
+
+void led_redInit(void) {
+// TODO
+}
+
+void led_redToggle(void) {
+// TODO
+}
+
+void led_redOn(void) {
+// TODO
+}
+
+void led_redOff(void) {
+// TODO
+}
+
+void led_yellowInit(void) {
+// TODO
+}
+
+void led_yellowToggle(void) {
+// TODO
+}
+
+void led_yellowOn(void) {
+// TODO
+}
+
+void led_yellowOff(void) {
+// TODO
+}
+
+void led_greenInit(void) {
+// TODO
+}
+
+void led_greenToggle(void) {
+// TODO
+}
+
+void led_greenOn(void) {
+// TODO
+}
+
+void led_greenOff(void) {
+// TODO
+}
